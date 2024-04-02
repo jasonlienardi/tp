f6b9b456111b5d9b6f0884dedcea2b416d7eccd8
Claribel Ho <hoclaribel@gmail.com>
2024-04-02 17:52:45 +0800

    Update help command with new methods

--- src/main/java/seedu/fitnus/parser/Parser.java
+++ src/main/java/seedu/fitnus/parser/Parser.java
@@ -227,6 +227,9 @@ public class Parser {
         System.out.println("- Add a drink: drink d/DRINK s/VOLUME(ML)");
         System.out.println("- Track an exercise: exercise e/EXERCISE d/DURATION(MINUTES) " +
                 "i/INTENSITY(HIGH, MEDIUM, LOW)");
+        System.out.println("- View all meals that you can input: allMeals");
+        System.out.println("- View all drinks that you can input: allDrinks");
+        System.out.println("- View all exercises that you can input: allExercises");
         System.out.println("- Find the information about a certain meal: infoMeal MEAL");
         System.out.println("- Find the information about a certain drink: infoDrink DRINK");
         System.out.println("- Find the information about a certain exercise: infoExercise EXERCISE");
