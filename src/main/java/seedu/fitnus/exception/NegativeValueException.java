9c1e37123c33cf5a0594e8a0cb8f254e499f7952
Claribel Ho <hoclaribel@gmail.com>
2024-04-02 18:31:49 +0800

    Add documentations to exception classes

--- src/main/java/seedu/fitnus/exception/NegativeValueException.java
+++ src/main/java/seedu/fitnus/exception/NegativeValueException.java
@@ -1,4 +1,8 @@
 package seedu.fitnus.exception;
 
+/**
+ * Exception class thrown when the integer is a non-positive value.
+ * Class is inherited from Exception.
+ */
 public class NegativeValueException extends Exception {
 }
--- src/main/java/seedu/fitnus/exception/UnregisteredExerciseException.java
+++ src/main/java/seedu/fitnus/exception/UnregisteredExerciseException.java
@@ -1,4 +1,8 @@
 package seedu.fitnus.exception;
 
+/**
+ * Exception class thrown when the specified exercise is not pre-defined.
+ * Class is inherited from Exception.
+ */
 public class UnregisteredExerciseException extends Exception {
 }
