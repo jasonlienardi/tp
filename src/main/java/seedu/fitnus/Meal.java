7a264194d91bee9d7d40fc0013f5a53db999f938
Claribel Ho <hoclaribel@gmail.com>
2024-04-02 18:20:54 +0800

    Add documentation for Drink and Meal

--- src/main/java/seedu/fitnus/Meal.java
+++ src/main/java/seedu/fitnus/Meal.java
@@ -18,6 +18,14 @@ public class Meal {
     private int fiber;
     private int sugar;
 
+    /**
+     * Constructor for a Meal, requires the name and serving size of the meal,
+     * as well as date that it is added into the list.
+     *
+     * @param name name of meal to be added into the list
+     * @param servingSize serving size of the meal to be added into the list
+     * @param currentDate date of which the meal is added into list
+     */
     public Meal(String name, int servingSize, String currentDate) {
         assert name != null : "Name must not be null";
         this.name = name;
@@ -43,38 +51,86 @@ public class Meal {
         sugar = nutrients[5] * servingSize;
     }
 
+    /**
+     * Returns a string stating the name of the meal.
+     *
+     * @return string stating the name of the meal
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Returns an integer value of the amount of calories in the meal.
+     *
+     * @return an integer value of the amount of calories in the meal
+     */
     public int getCalories() {
         return calories;
     }
 
+    /**
+     * Returns an integer value of the amount of carbohydrates in the meal.
+     *
+     * @return an integer value of the amount of carbohydrates in the meal
+     */
     public int getCarbs() {
         return carbs;
     }
 
+    /**
+     * Returns an integer value of the amount of protein in the meal.
+     *
+     * @return an integer value of the amount of protein in the meal
+     */
     public int getProtein() {
         return protein;
     }
 
+    /**
+     * Returns an integer value of the amount of fat in the meal.
+     *
+     * @return an integer value of the amount of fat in the meal
+     */
     public int getFat() {
         return fat;
     }
 
+    /**
+     * Returns an integer value of the amount of fiber in the meal.
+     *
+     * @return an integer value of the amount of fiber in the meal
+     */
     public int getFiber() {
         return fiber;
     }
 
+    /**
+     * Returns an integer value of the amount of sugar in the meal.
+     *
+     * @return an integer value of the amount of sugar in the meal
+     */
     public int getSugar() {
         return sugar;
     }
 
+    /**
+     * Returns an integer value of the serving size of the meal.
+     *
+     * @return an integer value of the serving size of the meal
+     */
     public int getServingSize() {
         return servingSize;
     }
 
+    /**
+     * Handles when user would like to find out the information about a certain meal.
+     * Prints out all nutritional content of the meal.
+     *
+     * @param command string inputted by user, containing meal to be viewed
+     * @throws UnregisteredMealException if meal specified is not a pre-defined meal
+     * @throws IncompleteInfoException if the user did not comply with the required format
+     */
     public static void handleInfoMeal(String command) throws UnregisteredMealException, IncompleteInfoException {
         String name = Parser.parseInfoMeal(command);
         int[] nutrients = nutrientDetails.get(name);
@@ -87,6 +143,10 @@ public class Meal {
         System.out.println("Sugar: " + nutrients[5]);
     }
 
+    /**
+     * Prints out all pre-defined meals in one line,
+     * only called when the user first enters the program.
+     */
     public static void printAvailableMeals() {
         System.out.print("Available meals: ");
         for (String meal : nutrientDetails.keySet()) {
@@ -97,6 +157,9 @@ public class Meal {
         System.out.println();
     }
 
+    /**
+     * Prints out all pre-defined meals in a list.
+     */
     public static void listAvailableMeals() {
         System.out.println("Available meals: ");
         for (String meal : nutrientDetails.keySet()) {
@@ -110,6 +173,11 @@ public class Meal {
         return nutrientDetails;
     }
 
+    /**
+     * Returns the date of which the meal was added into mealList.
+     *
+     * @return date of which the meal was added into mealList
+     */
     public String getDate() {
         return dateAdded;
     }
